6e67ccc112a696a416711cc885f2c68366c355c9
Eloá Franca Verona <eloafran@bitwise.io>
2020-02-02 11:14:56 -0600

    the mess of before

--- cli/src/action/circuit/mod.rs
+++ cli/src/action/circuit/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Cargill Incorporated
+// Copyright 2019 Cargill Incorporated
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,13 +14,17 @@
 
 mod api;
 pub mod defaults;
+mod file;
 mod payload;
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
-use clap::ArgMatches;
-use splinter::admin::messages::CreateCircuit;
+use clap::{ArgMatches, Values};
+use regex::Regex;
+use reqwest::Url;
+use splinter::admin::messages::{CreateCircuit, SplinterNode, SplinterService, AuthorizationType, PersistenceType, DurabilityType, RouteType};
 
 use crate::error::CliError;
 
@@ -35,16 +39,360 @@ impl Action for CircuitCreateAction {
         let key = args
             .value_of("private_key_file")
             .unwrap_or("./splinter.priv");
-        let path = match args.value_of("path") {
-            Some(path) => path,
-            None => return Err(CliError::ActionError("Path is required".into())),
-        };
+        if let Some(path) = args.value_of("path") {
+            create_circuit_proposal(url, key, path)
+        } else {
+            let mut nodes = match args.values_of("node") {
+                Some(nodes) => nodes,
+                None => return Err(CliError::ActionError("Path is required".into())),
+            };
 
-        create_circuit_proposal(url, key, path)
+            let members: Result<Vec<SplinterNode>, CliError> =
+                nodes.try_fold(Vec::new(), |mut acc, node| {
+                    let splinter_node = make_splinter_node(&node)?;
+                    acc.push(splinter_node);
+                    Ok(acc)
+                });
+
+            let mut services = match args.values_of("service") {
+                Some(mut services) => parse_service_arg(&mut services)?,
+                None => return Err(CliError::ActionError("Service is required".into())),
+            };
+
+            let mut service_argument = match args.values_of("service_argument") {
+                Some(mut arguments) => parse_service_argument(&mut arguments)?,
+                None => return Err(CliError::ActionError("Service is required".into())),
+            };
+
+            let mut auth_type = args.value_of("authorization_type").unwrap_or("trust");
+
+
+            let mut management_type = match args.value_of("management_type") {
+                Some(val) => val.to_string(),
+                None => {
+                    match defaults::get_default_management_type()?{
+                        Some(val) => val,
+                        None => return Err(CliError::ActionError(
+                            "Management-type not provided and no default set".into(),
+                        ))
+                    }
+                }
+
+            };
+
+            let mut service_types = match args.values_of("service_type") {
+                Some(mut service_type) => parse_sercive_type_arg(&mut service_type)?,
+                None => {
+                    let default_val = defaults::get_default_service_type()?;
+                    match default_val {
+                        Some(val) => {
+                            let service_id_match = Regex::new(".*").map_err(|_| {
+                                CliError::ActionError("Failed to set service-type".into())
+                            })?;
+                            vec![(service_id_match, val)]
+                        }
+                        None => {
+                            return Err(CliError::ActionError(
+                                "Service-type not provided and no default set".into(),
+                            ))
+                        }
+                    }
+                }
+            };
+
+            let services_with_type = assign_type_to_service(&services, &service_types)?;
+
+            let splinter_services = make_service_roster(&services_with_type, &service_argument);
+            warn!("nodes {:?}", members);
+            warn!("service_types {:?}", service_types);
+            warn!("services {:?}", splinter_services);
+            warn!("services_with_type {:?}", services_with_type);
+            warn!("service_argument {:?}", service_argument);
+            warn!("management_type {:?}", management_type);
+            warn!("auth_type {:?}", auth_type);
+
+            let create_circuit = CreateCircuit {
+                circuit_id: "adasda".to_string(),
+                roster: splinter_services,
+                members: members?,
+                authorization_type: AuthorizationType::Trust,
+                persistence: PersistenceType::Any,
+                durability: DurabilityType::NoDurability,
+                routes: RouteType::Any,
+                circuit_management_type: management_type,
+                application_metadata: vec![]
+            };
+
+            let client = api::SplinterRestClient::new(url);
+            let requester_node = client.fetch_node_id()?;
+            let private_key_hex = read_private_key(key)?;
+
+            // let proposal_file = File::open(proposal_path).map_err(|err| {
+            //     CliError::EnvironmentError(format!("Unable to open {}: {}", proposal_path, err))
+            // })?;
+            //
+            // let create_request: CreateCircuit = serde_yaml::from_reader(proposal_file).map_err(|err| {
+            //     CliError::EnvironmentError(format!("Unable to parse {}: {}", proposal_path, err))
+            // })?;
+
+            let signed_payload =
+                payload::make_signed_payload(&requester_node, &private_key_hex, create_circuit)?;
+
+            client.submit_admin_payload(signed_payload)?;
+
+            //warn!("services_with_type {:?}", services_with_type);
+
+            Ok(())
+        }
     }
 }
 
+fn parse_service_arg(service_type_values: &mut Values) -> Result<Vec<(String, String)>, CliError> {
+    service_type_values.try_fold(Vec::new(), |mut acc, val| {
+        let mut iter = val.split("::");
+
+        let service_id = iter
+            .next()
+            .ok_or_else(|| CliError::ActionError(format!("service_type not valid {}", val)))?
+            .to_string();
+
+        let service_type = iter
+            .next()
+            .ok_or_else(|| CliError::ActionError(format!("service_type not valid {}", val)))?
+            .to_string();
+        acc.push((service_id, service_type));
+        Ok(acc)
+    })
+}
+
+fn parse_service_argument(
+    service_arguments_values: &mut Values,
+) -> Result<Vec<(Regex, (String, Vec<String>))>, CliError> {
+    service_arguments_values.try_fold(Vec::new(), |mut acc, val| {
+        let mut iter = val.split("::");
+
+        let service_id = iter
+            .next()
+            .ok_or_else(|| CliError::ActionError(format!("service_type not valid {}", val)))?
+            .to_string();
+
+        let re = if service_id.starts_with("*") {
+            Regex::new(&format!(".{}", service_id)).map_err(|_| {
+                CliError::ActionError(format!("service_id is not valid {}", service_id))
+            })
+        } else {
+            Regex::new(&service_id).map_err(|_| {
+                CliError::ActionError(format!("service_id is not valid {}", service_id))
+            })
+        }?;
+
+        let arguments = iter
+            .next()
+            .ok_or_else(|| CliError::ActionError(format!("service_type not valid {}", val)))?
+            .to_string();
+        let mut argument_iter = arguments.split("=");
+        let key = argument_iter
+            .next()
+            .ok_or_else(|| CliError::ActionError(format!("service_type not valid {}", val)))?
+            .to_string();
+        let value = argument_iter
+            .next()
+            .ok_or_else(|| CliError::ActionError(format!("service_type not valid {}", val)))?
+            .split(",")
+            .map(String::from)
+            .collect();
+        acc.push((re, (key, value)));
+        Ok(acc)
+    })
+}
+
+fn parse_sercive_type_arg(
+    service_type_values: &mut Values,
+) -> Result<Vec<(Regex, String)>, CliError> {
+    service_type_values.try_fold(Vec::new(), |mut acc, val| {
+        let mut iter = val.split("::");
+
+        let service_id = iter
+            .next()
+            .ok_or_else(|| CliError::ActionError(format!("service_type not valid {}", val)))?
+            .to_string();
+
+        let re = if service_id.starts_with("*") {
+            Regex::new(&format!(".{}", service_id)).map_err(|_| {
+                CliError::ActionError(format!("service_id is not valid {}", service_id))
+            })
+        } else {
+            Regex::new(&service_id).map_err(|_| {
+                CliError::ActionError(format!("service_id is not valid {}", service_id))
+            })
+        }?;
+
+        let service_type = iter
+            .next()
+            .ok_or_else(|| CliError::ActionError(format!("service_type not valid {}", val)))?
+            .to_string();
+        acc.push((re, service_type));
+        Ok(acc)
+    })
+}
+
+fn assign_type_to_service(
+    services: &Vec<(String, String)>,
+    services_type: &Vec<(Regex, String)>,
+) -> Result<HashMap<String, Vec<(String, String)>>, CliError> {
+    services.iter().try_fold(
+        HashMap::<String, Vec<(String, String)>>::new(),
+        |mut acc, (service_id, allowed_nodes)| {
+            let service_type = services_type
+                .iter()
+                .find_map(|(service_id_match, service_type)| {
+                    if service_id_match.is_match(&service_id) {
+                        Some(service_type)
+                    } else {
+                        None
+                    }
+                })
+                .ok_or_else(|| {
+                    CliError::ActionError(format!(
+                        "No service type match for service {}",
+                        service_id
+                    ))
+                })?;
+
+            match acc.get_mut(service_type) {
+                Some(services) => {
+                    services.push((service_id.to_string(), allowed_nodes.to_string()))
+                }
+                None => {
+                    acc.insert(
+                        service_type.to_string(),
+                        vec![(service_id.to_string(), allowed_nodes.to_string())],
+                    );
+                }
+            }
+            Ok(acc)
+        },
+    )
+}
+
+fn make_service_roster(services: &HashMap<String, Vec<(String, String)>>, service_args: &Vec<(Regex, (String, Vec<String>))> ) -> Vec<SplinterService> {
+    // services.iter().try_fold(Vec::new(), |mut acc, (service_id, allowed_nodes)| {
+    //     let service_type = services_type.iter().find_map(|(service_id_match, service_type)| {
+    //         let re = Regex::new(service_id_match).unwrap();
+    //         if re.is_match(service_id) {
+    //             Some(service_type)
+    //         } else {
+    //             None
+    //         }
+    //     });
+    // })
+    services.keys().fold(Vec::new(), |mut acc, (service_type)| {
+
+        if let Some(services) = services.get(service_type) {
+            let peer_services = services
+                .iter()
+                .map(|(service_id, _)| service_id.to_string())
+                .collect::<Vec<String>>();
+            let mut splinter_services = services
+                .iter()
+                .enumerate()
+                .map(|(index, (service_id, allowed_nodes))| {
+                    warn!("peer_services {:?}", peer_services);
+                    let mut peers = peer_services.clone();
+                    peers.remove(index);
+                    warn!("PEERS {:?}", peers);
+                    warn!("INDEX {:?}", index);
+                    let mut args = service_args.iter().fold(Vec::new(), |mut acc, mut args| {
+                        if args.0.is_match(service_id) {
+                            let key = (args.1).0.to_owned();
+                            let val = format!("{:?}", (args.1).1);
+                            acc.push((key, val));
+                        };
+                        acc
+                    });
+                    args.push(("peer_services".into(), format!("{:?}", peers)));
+
+                    SplinterService {
+                        service_id: service_id.to_string(),
+                        service_type: service_type.to_string(),
+                        allowed_nodes: vec![allowed_nodes.into()],
+                        arguments: args,
+                    }
+                })
+                .collect::<Vec<SplinterService>>();
+            acc.append(&mut splinter_services);
+        }
+        acc
+    })
+}
+
+fn make_splinter_node(value: &str) -> Result<SplinterNode, CliError> {
+    match Url::parse(value) {
+        Ok(url) => {
+            let host = match url.host_str() {
+                Some(host) => host,
+                None => {
+                    return Err(CliError::ActionError(format!(
+                        "Invalid node endpoint: {}",
+                        value
+                    )))
+                }
+            };
+            let port = match url.port_or_known_default() {
+                Some(port) => port,
+                None => {
+                    return Err(CliError::ActionError(format!(
+                        "Invalid node endpoint: {}",
+                        value
+                    )))
+                }
+            };
+            Ok(SplinterNode {
+                node_id: format!("{}_{}", host, port),
+                endpoint: value.to_string(),
+            })
+        }
+        Err(err) => match node::get_endpoint_for_alias(value)? {
+            Some(endpoint) => Ok(SplinterNode {
+                node_id: value.to_string(),
+                endpoint: endpoint.to_string(),
+            }),
+            None => Err(CliError::ActionError(format!(
+                "Invalid node endpoint or node alias has not been set: {}",
+                value
+            ))),
+        },
+    }
+}
+
+// fn generate_node_id(enpoint: &str) -> String {
+//
+// }
+
 fn create_circuit_proposal(
+    url: &str,
+    private_key_file: &str,
+    proposal_path: &str,
+) -> Result<(), CliError> {
+    let client = api::SplinterRestClient::new(url);
+    let requester_node = client.fetch_node_id()?;
+    let private_key_hex = read_private_key(private_key_file)?;
+
+    let proposal_file = File::open(proposal_path).map_err(|err| {
+        CliError::EnvironmentError(format!("Unable to open {}: {}", proposal_path, err))
+    })?;
+
+    let create_request: CreateCircuit = serde_yaml::from_reader(proposal_file).map_err(|err| {
+        CliError::EnvironmentError(format!("Unable to parse {}: {}", proposal_path, err))
+    })?;
+
+    let signed_payload =
+        payload::make_signed_payload(&requester_node, &private_key_hex, create_request)?;
+
+    client.submit_admin_payload(signed_payload)
+}
+
+fn create_circuit_proposal_no_file(
     url: &str,
     private_key_file: &str,
     proposal_path: &str,
@@ -74,12 +422,12 @@ pub fn read_private_key(file_name: &str) -> Result<String, CliError> {
     })?;
 
     let mut buf = String::new();
+
     file.read_to_string(&mut buf).map_err(|err| {
         CliError::EnvironmentError(format!("Unable to read {}: {}", file_name, err))
     })?;
-    let key = buf.trim().to_string();
 
-    Ok(key)
+    Ok(buf)
 }
 
 pub(self) enum Vote {
