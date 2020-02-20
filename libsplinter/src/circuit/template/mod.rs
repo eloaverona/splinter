e2fe89fa707583da209b6ec3a9c8b357e0b0fbc6
Eloá Franca Verona <eloafran@bitwise.io>
2020-02-20 13:37:04 -0600

    Add method to convert template into builders
    Signed-off-by: Eloá Franca Verona <eloafran@bitwise.io>

--- libsplinter/src/circuit/template/mod.rs
+++ libsplinter/src/circuit/template/mod.rs
@@ -16,7 +16,11 @@ mod error;
 mod v0_1;
 mod yaml_parser;
 
+use std::collections::HashMap;
+
 pub use error::Error;
+
+use yaml_parser::{load_template, Template};
 
 pub(self) use crate::admin::messages::{CreateCircuitBuilder, SplinterServiceBuilder};
 
@@ -26,6 +30,24 @@ pub struct Builders {
 }
 
 impl Builders {
+    pub fn try_from_template(
+        path: &str,
+        arguments: &HashMap<String, String>,
+    ) -> Result<Builders, Error> {
+        let template = load_template(path)?;
+        let mut builders = Builders {
+            create_circuit_builder: CreateCircuitBuilder::new(),
+            service_builders: vec![],
+        };
+        match template {
+            Template::V0_1(template) => {
+                let native_template = v0_1::CircuitCreateTemplate::from(template);
+                native_template.apply_rules(&mut builders, &arguments)?;
+            }
+        }
+        Ok(builders)
+    }
+
     pub fn set_create_circuit_builder(&mut self, builder: CreateCircuitBuilder) {
         self.create_circuit_builder = builder;
     }
@@ -40,5 +62,70 @@ impl Builders {
 
     pub fn service_builders(&self) -> Vec<SplinterServiceBuilder> {
         self.service_builders.clone()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::fs::{remove_file, File};
+    use std::io::Write;
+    use std::{env, panic, thread};
+
+    static EXAMPLE_TEMPLATE_YAML: &[u8; 167] = br##"version: v0.1
+args:
+    - name: admin-keys
+      required: true
+      default: $(a:SIGNER_PUB_KEY)
+rules:
+    set-management-type:
+        management-type: "gameroom" "##;
+
+    /*
+     * Verifies that Builders can be parsed from template v0.1 and correctly
+     * applies the set-management-type rule
+     */
+    #[test]
+    fn test_builds_template_v0_1() {
+        run_test(|test_yaml_file_path| {
+            write_yaml_file(test_yaml_file_path);
+            let builders = Builders::try_from_template(test_yaml_file_path, &HashMap::new())
+                .expect("Error getting builders from templates");
+            let circuit_create_builder = builders.create_circuit_builder();
+            assert_eq!(
+                circuit_create_builder.circuit_management_type(),
+                Some("gameroom".to_string())
+            );
+        })
+    }
+
+    fn run_test<T>(test: T) -> ()
+    where
+        T: FnOnce(&str) -> () + panic::UnwindSafe,
+    {
+        let test_yaml_file = temp_yaml_file_path();
+
+        let test_path = test_yaml_file.clone();
+        let result = panic::catch_unwind(move || test(&test_path));
+
+        remove_file(test_yaml_file).unwrap();
+
+        assert!(result.is_ok())
+    }
+
+    fn temp_yaml_file_path() -> String {
+        let mut temp_dir = env::temp_dir();
+
+        let thread_id = thread::current().id();
+        temp_dir.push(format!("test_parse_template-{:?}.yaml", thread_id));
+        temp_dir.to_str().unwrap().to_string()
+    }
+
+    fn write_yaml_file(file_path: &str) {
+        println!("file_path {}", file_path);
+        let mut file = File::create(file_path).expect("Error creating test template yaml file.");
+
+        file.write_all(EXAMPLE_TEMPLATE_YAML)
+            .expect("Error writing example template yaml.");
     }
 }
